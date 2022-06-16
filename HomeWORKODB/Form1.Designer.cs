09cecb15742cf202410879c71a41d477c70ffcf1
whyalwayswannadie <v.v.lopatka@student.khai.edu>
2022-06-16 12:53:03 +0300

    ODB HW

---
+++ HomeWORKODB/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace HomeWORKODB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button buttonUpdate;
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.BrokerID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BrokageCompany = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxBrokageCompany = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            buttonUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonUpdate
+            // 
+            buttonUpdate.BackColor = System.Drawing.Color.LightGreen;
+            buttonUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            buttonUpdate.Location = new System.Drawing.Point(16, 269);
+            buttonUpdate.Name = "buttonUpdate";
+            buttonUpdate.Size = new System.Drawing.Size(217, 55);
+            buttonUpdate.TabIndex = 4;
+            buttonUpdate.Text = "Update";
+            buttonUpdate.UseCompatibleTextRendering = true;
+            buttonUpdate.UseVisualStyleBackColor = false;
+            buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.MediumAquamarine;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.BrokerID,
+            this.FirstName,
+            this.LastName,
+            this.BrokageCompany});
+            this.dataGridView1.Location = new System.Drawing.Point(540, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(961, 452);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // BrokerID
+            // 
+            this.BrokerID.DataPropertyName = "BrokerID";
+            this.BrokerID.HeaderText = "BrokerID";
+            this.BrokerID.MinimumWidth = 8;
+            this.BrokerID.Name = "BrokerID";
+            this.BrokerID.Visible = false;
+            this.BrokerID.Width = 150;
+            // 
+            // FirstName
+            // 
+            this.FirstName.DataPropertyName = "FirstName";
+            this.FirstName.HeaderText = "FirstName";
+            this.FirstName.MinimumWidth = 8;
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Width = 150;
+            // 
+            // LastName
+            // 
+            this.LastName.DataPropertyName = "LastName";
+            this.LastName.HeaderText = "LastName";
+            this.LastName.MinimumWidth = 8;
+            this.LastName.Name = "LastName";
+            this.LastName.Width = 150;
+            // 
+            // BrokageCompany
+            // 
+            this.BrokageCompany.DataPropertyName = "BrokageCompany";
+            this.BrokageCompany.HeaderText = "BrokageCompany";
+            this.BrokageCompany.MinimumWidth = 8;
+            this.BrokageCompany.Name = "BrokageCompany";
+            this.BrokageCompany.Width = 150;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelName.Location = new System.Drawing.Point(30, 30);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(113, 26);
+            this.labelName.TabIndex = 7;
+            this.labelName.Text = "FirstName";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxName.Location = new System.Drawing.Point(204, 32);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(288, 32);
+            this.textBoxName.TabIndex = 9;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.BackColor = System.Drawing.Color.Navy;
+            this.buttonCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCancel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonCancel.Location = new System.Drawing.Point(104, 404);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(275, 55);
+            this.buttonCancel.TabIndex = 10;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseCompatibleTextRendering = true;
+            this.buttonCancel.UseVisualStyleBackColor = false;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxLastName.Location = new System.Drawing.Point(204, 87);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(288, 32);
+            this.textBoxLastName.TabIndex = 11;
+            // 
+            // textBoxBrokageCompany
+            // 
+            this.textBoxBrokageCompany.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxBrokageCompany.Location = new System.Drawing.Point(204, 141);
+            this.textBoxBrokageCompany.Name = "textBoxBrokageCompany";
+            this.textBoxBrokageCompany.Size = new System.Drawing.Size(288, 32);
+            this.textBoxBrokageCompany.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(31, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 26);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "LastName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(11, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(187, 26);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "BrokageCompany";
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDelete.Location = new System.Drawing.Point(294, 269);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(197, 55);
+            this.buttonDelete.TabIndex = 15;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseCompatibleTextRendering = true;
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(1542, 527);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxBrokageCompany);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(buttonUpdate);
+            this.Name = "Form1";
+            this.Text = "Broker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxBrokageCompany;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BrokerID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BrokageCompany;
+        private System.Windows.Forms.Button buttonDelete;
+    }
+}
+
---
+++ HomeWORKODB/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NLog;
+namespace HomeWORKODB
+{
+    public partial class Form1 : Form
+
+    {
+
+        Broker model = new Broker();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Clear();
+            PopulateDataGridView();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            Clear(); 
+        }
+        void Clear()
+        {
+            textBoxName.Text = textBoxLastName.Text = textBoxBrokageCompany.Text = "";
+            buttonDelete.Enabled = true;
+            model.BrokerID = 0;
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+           
+            model.FirstName = textBoxName.Text.Trim();
+            model.LastName = textBoxLastName.Text.Trim();
+
+            model.BrokageCompany = textBoxBrokageCompany.Text.Trim();
+
+            model.FirstName = textBoxName.Text.Trim();
+
+
+            using (HWDBEntities1 db = new HWDBEntities1())
+            {
+                if(model.BrokerID == 0)//Insert
+                {
+                    db.Broker.Add(model);
+
+                }
+                else//update
+                
+               db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+            Clear();
+            PopulateDataGridView();
+            MessageBox.Show("Submitted Successfully");
+        }
+        void PopulateDataGridView()
+        {
+            using (HWDBEntities1 db = new HWDBEntities1())
+            {
+                dataGridView1.DataSource = db.Broker.ToList<Broker>();
+            }
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            if(dataGridView1.CurrentRow.Index != -1)
+            {
+                model.BrokerID = Convert.ToInt32(dataGridView1.CurrentRow.Cells["BrokerID"].Value);
+                   using(HWDBEntities1 db = new HWDBEntities1())
+                {
+                    model = db.Broker.Where(x => x.BrokerID == model.BrokerID).FirstOrDefault();
+                    textBoxName.Text = model.FirstName;
+                    textBoxLastName.Text = model.LastName;
+
+                    textBoxBrokageCompany.Text = model.BrokageCompany;
+
+                }
+     
+                buttonDelete.Text = "Delete";  
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+    
+
+        private void buttonDelete_Click_1(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure to delete this record?",
+              "EF CRUD OPERATION", MessageBoxButtons.YesNo) ==
+              DialogResult.Yes)
+            {
+                using (HWDBEntities1 db = new HWDBEntities1())
+                {
+                    var entry = db.Entry(model);
+                    if (entry.State == System.Data.Entity.EntityState.Detached)
+                        db.Broker.Attach(model);
+                    db.Broker.Remove(model);
+                    db.SaveChanges();
+                    PopulateDataGridView();
+                    Clear();
+                    MessageBox.Show("Deleted successfully!");
+                }
+            }
+        }
+    }
+}
